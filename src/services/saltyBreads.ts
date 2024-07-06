a8356da7f6e66454d2b70ac4963a95eed8fc7358
jeflugo <jefersoneduardoluego@gmail.com>
2024-07-05 23:28:17 -0400

    Services done to work with client

--- src/services/saltyBreads.ts
+++ src/services/saltyBreads.ts
@@ -1,15 +1,53 @@
 import SaltyBread from '../models/saltyBreads'
 import { TBread } from '../types'
 
-const getBreads = async () => await SaltyBread.find()
+const getBreads = async () => {
+	const newBreads = await SaltyBread.find()
+	const newBreadsModified = newBreads.map(
+		({ id, name, weight, left, make }) => {
+			return {
+				id,
+				name,
+				weight,
+				left,
+				make,
+			}
+		}
+	)
+	return newBreadsModified
+}
 
-const insertBread = async (bread: TBread) => await SaltyBread.create(bread)
+const insertBread = async (bread: TBread) => {
+	const newBread = await SaltyBread.create(bread)
+	const { name, weight, left, make, id } = newBread
+	return {
+		id,
+		name,
+		weight,
+		left,
+		make,
+	}
+}
 
 const getBread = async (id: string) => await SaltyBread.findById(id)
 
-const updateBread = async (id: string, updates: Partial<TBread>) =>
-	await SaltyBread.findByIdAndUpdate(id, updates, { new: true })
+const updateBread = async (id: string, updates: Partial<TBread>) => {
+	const updatedBread = await SaltyBread.findByIdAndUpdate(id, updates, {
+		new: true,
+	})
+	const { _id, name, weight, left, make } = updatedBread!
+	return {
+		id: _id,
+		name,
+		weight,
+		left,
+		make,
+	}
+}
 
-const deleteBread = async (id: string) => await SaltyBread.findByIdAndDelete(id)
+const deleteBread = async (id: string) => {
+	const deletedBread = await SaltyBread.findByIdAndDelete(id)
+	return { id: deletedBread?.id }
+}
 
 export default { insertBread, getBreads, getBread, updateBread, deleteBread }
--- src/services/sweetBreads.ts
+++ src/services/sweetBreads.ts
@@ -1,15 +1,52 @@
 import SweetBread from '../models/sweetBreads'
 import { TBread } from '../types'
 
-const getBreads = async () => await SweetBread.find()
+const getBreads = async () => {
+	const newBreads = await SweetBread.find()
+	const newBreadsModified = newBreads.map(
+		({ _id, name, weight, left, make }) => {
+			return {
+				id: _id,
+				name,
+				weight,
+				left,
+				make,
+			}
+		}
+	)
+	return newBreadsModified
+}
 
-const insertBread = async (bread: TBread) => await SweetBread.create(bread)
+const insertBread = async (bread: TBread) => {
+	const { _id, name, weight, left, make } = await SweetBread.create(bread)
+	return {
+		id: _id,
+		name,
+		weight,
+		left,
+		make,
+	}
+}
 
 const getBread = async (id: string) => await SweetBread.findById(id)
 
-const updateBread = async (id: string, updates: Partial<TBread>) =>
-	await SweetBread.findByIdAndUpdate(id, updates, { new: true })
+const updateBread = async (id: string, updates: Partial<TBread>) => {
+	const updatedBread = await SweetBread.findByIdAndUpdate(id, updates, {
+		new: true,
+	})
+	const { _id, name, weight, left, make } = updatedBread!
+	return {
+		id: _id,
+		name,
+		weight,
+		left,
+		make,
+	}
+}
 
-const deleteBread = async (id: string) => await SweetBread.findByIdAndDelete(id)
+const deleteBread = async (id: string) => {
+	const deletedBread = await SweetBread.findByIdAndDelete(id)
+	return { id: deletedBread?._id }
+}
 
 export default { insertBread, getBreads, getBread, updateBread, deleteBread }
